65499d2c83ce1479874dd997adeeb8d72dd7b5c1
art-gar <72654944+Art-Gar@users.noreply.github.com>
2023-02-27 04:58:05 +0200

    Changed members

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const members= require('../../Members');
 const uuid = require('uuid');
+
+const fs = require('fs');
+const membersDir='../../Members.json';
+const membersFile= require(membersDir);
+const members=membersFile["members"];
+
+
 // Gets all members
 router.get('/', (req, res) =>  res.json(members));
 
 // Get single member
+
 router.get('/:id', (req,res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
+    const found = members.some(member => member.id.toString() === req.params.id.toString());
     if(found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    res.json(members.filter(member => member.id.toString() === req.params.id.toString()));
     } else{
-        res.status(400).json({ msg: `No member with the id of ${req.params.id} found`})
+        res.status(400).json({ msg: `No member with the id of ${req.params.id.toString()} found`})
     }
 
 });
@@ -27,26 +34,37 @@ router.post('/', (req, res) => {
     if(!newMember.name || !newMember.email) {
         res.status(400).json({msg: 'Please include both a name and an email'});
     }
+    else{
+    membersFile["members"].push(newMember);
+    fs.writeFile('Members.json', JSON.stringify(membersFile,null,2), function writeJSON(err) {
+        if (err) return console.log(err);
 
-    members.push(newMember);
-    res.json(members);
-
+        console.log('writing to ' + membersDir);
+      });
+    res.json(membersFile);
+    }
     //sends back a response of the body from post request
     // res.send(req.body);
 });
 
+
 // Update Member
-// Get single member
 router.put('/:id', (req,res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
+    const found = members.some(member => member.id.toString() === req.params.id.toString());
     if(found){
     const updMember = req.body;
     members.forEach(member => {
-        if(member.id === parseInt(req.params.id)) {
+        if(member.id.toString() === req.params.id.toString()) {
             member.name = updMember.name ? updMember.name : member.name;
             member.email = updMember.email ? updMember.email : member.email;
+            fs.writeFile('Members.json', JSON.stringify(membersFile,null,2), function writeJSON(err) {
+                if (err) return console.log(err);
+        
+                console.log('writing to ' + membersDir);
+              });
 
             res.json({ msg: 'Member updated', member})
+
         }
 
     });
@@ -57,17 +75,27 @@ router.put('/:id', (req,res) => {
 });
 
 
+
+
 // Delete Member
 router.delete('/:id', (req,res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
+    const found = members.some(member => member.id.toString() === req.params.id.toString());
     if(found){
+        membersFile["members"]=members.filter(member => member.id.toString() !== req.params.id.toString());
+        fs.writeFile('Members.json', JSON.stringify(membersFile,null,2), function writeJSON(err) {
+            if (err) return console.log(err);
+
+            console.log('writing to ' + membersDir);
+          });
+        
+        // console.log(membersFile);
     res.json({  msg: `member deleted`,
-                members: members.filter(member => member.id !== parseInt(req.params.id)),
+                 members: members.filter(member => member.id.toString() !== req.params.id.toString()),
                 
     });
     } else{
         res.status(400).json({ msg: `No member with the id of ${req.params.id} found`})
     }
 
-});
+}); 
 module.exports=router;
